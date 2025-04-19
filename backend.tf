a757d5f0575121c6fac1ab9de11099f789424b18
Deepak Kumar <128142357+devops-aws-geek@users.noreply.github.com>
2025-04-19 13:18:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-bucket-batch891"
+    bucket         = "tf-bucket-batch910"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "tf-lock-table-batch891"
+    dynamodb_table = "tf-lock-table-910"
     encrypt        = true
   }
 }
