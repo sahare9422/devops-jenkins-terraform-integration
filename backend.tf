e6c4c6f247297ef525075aad02e2021e4fb2fac3
Palash Sahare <128880606+sahare9422@users.noreply.github.com>
2025-04-23 10:14:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-bucket-batch910"
+    bucket         = "tf-bucket-palash9422"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf-lock-table-910"
